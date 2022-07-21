c33588983e7072c1817473ffc3d0823eac701205
suruchisinha30 <suruchisinha.3011@gmail.com>
2022-07-21 15:25:31 +0530

    first commit

---
+++ src/test/java/io/javabrains/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
